4d68dc568f029869b68f71e8a7bd79a2525d524d
Julie Pan <ycp217@nyu.edu>
2015-08-15 02:22:18 -0700

    add some models

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,21 @@
+Parse.initialize(getParseKey1(), getParseKey2());
+var div = document.createElement('div');
+div.setAttribute('id', 'container');
+
+$('body').append(div);
+
 console.log('\'Allo \'Allo!');
 
-Parse.initialize(getParseKey1(), getParseKey2());
+
 // PARSE TEST CODE
 // var user = new Parse.User();
 // user.set("username", "my name");
 // user.set("password", "my pass");
 // user.set("email", "email@example.com");
-  
+
 // // other fields can be set just like with Parse.Object
 // user.set("phone", "650-555-0000");
-  
+
 // user.signUp(null, {
 //   success: function(user) {
 //     // Hooray! Let them use the app now.
@@ -19,3 +25,51 @@ Parse.initialize(getParseKey1(), getParseKey2());
 //     alert("Error: " + error.code + " " + error.message);
 //   }
 // });
+
+var username = 'my name';
+var password = 'my pass';
+
+Parse.User.logIn(username, password, {
+  success: function(user) {
+    console.log('logged in');
+    loggedIn(user);
+  },
+  error: function(user, error) {
+    console.log('not logged in');
+    // redirect to make user page
+  }
+});
+
+function loggedIn(user) {
+  // console.log(user);
+  // get ALL postings
+  // 
+}
+
+var Post = Backbone.Model.extend({
+  defaults: {
+    description: '',
+    username: '',
+    picture: '',
+    price: 0,
+    quantity: 0
+  }
+});
+
+var User = Backbone.Model.extend({
+  defaults: {
+    username: '',
+    password: '',
+    email: ''
+  }
+});
+
+
+var user1 = new User({
+  username: 'testuser',
+  password: 'testpassword',
+  email: 'ycp217@nyu.edu'
+});
+console.log(user1.toJSON());
+
+
