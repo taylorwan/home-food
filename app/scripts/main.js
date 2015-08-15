a3255ef623c3cc059ef35f5fa253418e1b259b29
Julie Pan <ycp217@nyu.edu>
2015-08-15 00:00:42 -0700

    connect Parse

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
 console.log('\'Allo \'Allo!');
+
+Parse.initialize(getParseKey1(), getParseKey2());
+// PARSE TEST CODE
+// var user = new Parse.User();
+// user.set("username", "my name");
+// user.set("password", "my pass");
+// user.set("email", "email@example.com");
+  
+// // other fields can be set just like with Parse.Object
+// user.set("phone", "650-555-0000");
+  
+// user.signUp(null, {
+//   success: function(user) {
+//     // Hooray! Let them use the app now.
+//   },
+//   error: function(user, error) {
+//     // Show the error message somewhere and let the user try again.
+//     alert("Error: " + error.code + " " + error.message);
+//   }
+// });
