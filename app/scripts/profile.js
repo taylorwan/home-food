46e769806cb6cd8054e6e2634838b943d6a1a2c6
taylor <taylor@splashthat.com>
2015-08-15 15:14:12 -0700

    working profile with sample data

--- app/scripts/profile.js
+++ app/scripts/profile.js
@@ -1,6 +1,44 @@
-var currentUser = Parse.User.current();
-if (currentUser) {
-    // do stuff with the user
-} else {
-    // show the signup or login page
-}
+var React = require('react');
+var Posts = require('./posts');
+var Orders = require('./orders');
+
+var React = require('react');
+
+var samplePosts = [{
+  name: 'Tamales', 
+  price: '$3/unit', 
+  availability: '2 days notice'
+}, {
+  name: 'Tamales', 
+  price: '$3/unit', 
+  availability: '2 days notice'
+}];
+
+var sampleOrders = [{
+	name: 'Tamales',
+	price: '$3/unit',
+	qty: '20',
+	ddate: '09/20/15'
+}];
+
+var Profile = React.createClass({
+  render: function() {
+    return (
+      <div className="content">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="profile-img-holder">
+                <img src="img/t.jpg" className="img-responsive img-circle text-center"/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Posts limit={3}/>
+        <Orders limit={3}/>
+      </div>
+    )
+  }
+});
+
+module.exports = Profile;
