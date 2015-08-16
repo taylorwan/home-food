e9127f38938feadbe0a98c9965f8939388de27db
Julie Pan <ycp217@nyu.edu>
2015-08-16 00:49:17 -0700

    filter by user set

--- app/scripts/profile.js
+++ app/scripts/profile.js
@@ -16,30 +16,58 @@ var samplePosts = [{
 }];
 
 var sampleOrders = [{
-	name: 'Tamales',
-	price: '$3/unit',
-	qty: '20',
-	ddate: '09/20/15'
+  name: 'Tamales',
+  price: '$3/unit',
+  qty: '20',
+  ddate: '09/20/15'
 }];
+
+var transition = function() {
+  $('.collapse').collapse("hide");
+  return true;
+};
 
 var Profile = React.createClass({
   getInitialState: function() {
-    return {user: Parse.User.current() };
+    return {
+      user: Parse.User.current(),
+      posts: []
+    };
+  },
+  componentDidMount: function() {
+    var user = Parse.User.current();
+    var query = new Parse.Query('Post');
+    query.equalTo('user', user);
+    query.find().then(function(res) {
+      console.log(res);
+      res = res.map(function(el) {
+        if(el.get('imageFile')) el.set('imageLink', el.get('imageFile').url());
+        return el.toJSON();
+      });
+      this.setState({
+        posts: res
+      });
+
+    }.bind(this));
   },
   render: function() {
+    console.log(this.props.name);
     return (
       <div className="content">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <div className="profile-img-holder">
-                <img src="images/t.jpg" className="img-responsive img-circle text-center"/>
-              </div>
-            </div>
-          </div>
-        </div>
-        <Posts limit={3} orderable={false} />
-        <Orders limit={3}/>
+      <div className="container">
+      <div className="row">
+      <div className="col-md-12">
+      <div className="profile-img-holder">
+      <img src="images/t.jpg" className="img-responsive img-circle text-center"/>
+      </div>
+
+      </div>
+      </div>
+      </div>
+
+
+      <Posts limit={3} orderable={false} />
+      <Orders limit={3}/>
       </div>
     )
   }
